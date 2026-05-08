aa275daa870c93379c717fd5881d7c6b204f79f9
chistisaicharan <chistisaicharan@gmail.com>
2026-05-08 12:24:19 +0530

    feat: Role Authorization Middleware

--- routes/auth_route.js
+++ routes/auth_route.js
@@ -5,8 +5,36 @@ const express=require("express")
 
 const routes=express.Router();
 
+const authMiddleware=require("../middleware/auth.middleware");
+
 
 routes.post("/users",registerUser);
-routes.post("/login",userLogin);
+routes.post("/login",authMiddleware,userLogin);
+
+
+// Only doctor can access
+routes.get(
+  "/doctor-dashboard",
+  authMiddleware("Doctor"),
+  (req, res) => {
+    res.json({
+      success: true,
+      message: "Welcome Doctor",
+    });
+  }
+);
+
+
+// Doctor and admin both can access
+routes.get(
+  "/patients",
+  authMiddleware("Doctor", "Admin"),
+  (req, res) => {
+    res.json({
+      success: true,
+      message: "Patient Records",
+    });
+  }
+);
 
 module.exports=routes;
